7b81e61ce8c2a845d03c3ba401a0e983c5a237c8
Brian Becerra <elibecerrahdz@gmail.com>
2022-08-25 05:59:05 -0500

    25/08/2022

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,33 +1,33 @@
 import './Expenses.css';
 
+import { useState } from 'react';
+
 import Card from '../UI/Card';
 import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter';
 
 function Expenses(props) {
     var expenses = props.expenses;
 
+    var [filter, setFilter] = useState('2020');
+
+    function filterChangeHandler(selectedFilter) {
+        setFilter(selectedFilter);
+        console.log(selectedFilter);
+    }
+
     return (
         <Card className="expenses">
-            <ExpenseItem
-                title={expenses[0].title}
-                amount={expenses[0].amount}
-                date={expenses[0].date}
-            />
-            <ExpenseItem
-                title={expenses[1].title}
-                amount={expenses[1].amount}
-                date={expenses[1].date}
-            />
-            <ExpenseItem
-                title={expenses[2].title}
-                amount={expenses[2].amount}
-                date={expenses[2].date}
-            />
-            <ExpenseItem
-                title={expenses[3].title}
-                amount={expenses[3].amount}
-                date={expenses[3].date}
-            />
+            <ExpensesFilter selectedFilter={filter} onChangeFilter={filterChangeHandler}/>
+            {expenses.map(function (expense) {
+                return (
+                    <ExpenseItem
+                        title={expense.title}
+                        amount={expense.amount}
+                        date={expense.date}
+                    />
+                );
+            })}
         </Card>
     );
 }
